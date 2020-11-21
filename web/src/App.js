dfd805c90e089c5688311b67249f50344719498a
PedroFerr101 <pferreira59@gmail.com>
2020-11-21 03:16:10 +0000

    Add Tuichain name

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <span>Learn React</span>
+        <span>Tuichain</span>
       </div>
     );
   }
