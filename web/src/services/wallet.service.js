407066d3ac4af06d7ba4a240bfbccef553badc30
joaop21 <jpsilva9898@gmail.com>
2021-01-03 14:53:37 +0000

    Add Wallet Service

---
+++ web/src/services/wallet.service.js
@@ -0,0 +1,81 @@
+//import Web3 from "web3";
+
+/**
+ * Function that checks if metamask exists.
+ * 
+ * @returns The ethereum property of a widow, or an error
+ */
+function checkConnection() {
+
+  const { ethereum } = window;
+
+  if(!ethereum) {
+    throw new Error("Metamask not found");
+  }
+
+  return ethereum;
+
+}
+
+/**
+ * Function for checking if an account is connected
+ * 
+ * @returns The selected account of the connected wallet
+ */
+function checkAccount() {
+
+  const ethereum = checkConnection();
+
+  return ethereum.selectedAddress
+
+}
+
+/**
+ * Function for establish a connection with Metamask
+ */
+async function connectToWallet() {
+
+  const ethereum = checkConnection();
+
+  if(checkAccount() == null) {
+
+    try {
+
+      await ethereum.send('eth_requestAccounts');
+      
+    } catch (error) {
+      console.log(error);
+      //return connectToWallet()
+    }
+
+  } else {
+
+    console.log('account: ' + ethereum.selectedAddress);
+
+  }
+
+}
+
+/**
+ * Function that listens account changing
+ * 
+ * @param setWallet Function to change the connected wallet account
+ */
+function changeAccounts(setWallet) {
+
+  const ethereum = checkConnection();
+
+  ethereum.on('accountsChanged', function (accounts) {
+    
+    setWallet(accounts[0]);
+
+  });
+  
+}
+
+export default {
+  checkAccount,
+  connectToWallet,
+  changeAccounts
+};
+
