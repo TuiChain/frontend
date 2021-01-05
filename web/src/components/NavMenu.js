f1c3eab98fcc9108c082d09cf776cbab3cfac958
João <jpsilva9898@gmail.com>
2021-01-05 01:55:45 +0000

    Metamask Connection (#25)
    * Add Wallet service
    
    * Add Wallet Service
    
    * Remove web3
    
    * Remove a comment
    
    * Revert package-lock.json
    
    * Ran prettier
    
    * Change connect button for logged in state
    
    * Change import
    
    * Change to Connected

--- web/src/components/NavMenu.js
+++ web/src/components/NavMenu.js
@@ -1,16 +1,36 @@
 /* eslint react/prop-types: 0 */
 import { React, useState } from "react";
+import { Button } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import MenuIcon from "@material-ui/icons/Menu";
 //import AuthService from "../services/auth.service";
+import WalletService from "../services/wallet.service";
 import { useHistory } from "react-router-dom";
 
 const NavMenu = (props) => {
   const { onLogout } = props;
   const [anchorEl, setAnchorEl] = useState(null);
   const history = useHistory();
+
+  const [wallet, setWallet] = useState(WalletService.checkAccount);
+  WalletService.changeAccounts(setWallet);
+
+  const connect_button =
+    wallet == null ? (
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => WalletService.connectToWallet()}
+      >
+        Connect Wallet
+      </Button>
+    ) : (
+      <Button variant="contained" color="secondary" disabled>
+        Connected
+      </Button>
+    );
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -27,6 +47,7 @@ const NavMenu = (props) => {
 
   return (
     <div>
+      {connect_button}
       <IconButton
         color="secondary"
         aria-label="menu"
