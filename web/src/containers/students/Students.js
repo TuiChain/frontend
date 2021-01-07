1f117b46e6649ce9b1e2932a2efa92f27d035388
PedroFerr101 <pferreira59@gmail.com>
2021-01-07 00:49:40 +0000

    Add search bar

--- web/src/containers/students/Students.js
+++ web/src/containers/students/Students.js
@@ -1,43 +1,84 @@
 import React, { useState, useEffect } from "react";
 import getStudents from "../../services/students.service";
-import Grid from "@material-ui/core/Grid";
+import { Grid, Input } from "@material-ui/core";
 import { styled } from "@material-ui/core/styles";
 import StudentCard from "../../components/StudentCard";
-import theme from '../../theme';
+import theme from "../../theme";
+import PropTypes from "prop-types";
 
-const StyledGrid = styled(Grid)({
+const StudentsGrid = styled(Grid)({
   paddingLeft: "10%",
   paddingRight: "10%",
 
-  [theme.breakpoints.only('sm')]: {
+  [theme.breakpoints.only("sm")]: {
     paddingLeft: "30%",
     paddingRight: "30%",
   },
-  [theme.breakpoints.only('xs')]: {
+  [theme.breakpoints.only("xs")]: {
     paddingLeft: "15%",
     paddingRight: "15%",
   },
 });
 
+const SearchArea =  styled(Grid)({
+  paddingLeft: "10%",
+  paddingRight: "10%",
+  paddingBottom: "4%",
+  paddingTop: "2%",
+})
+
+const SearchBar = ({ input: keyword, onChange: setKeyword }) => {
+  const BarStyling = {
+    background: "#EFF0F6",
+    padding: "0.7rem",
+    borderRadius: 20,
+  };
+  return (
+    <Input
+      fullWidth
+      disableUnderline
+      style={BarStyling}
+      value={keyword}
+      placeholder={"Search for a student..."}
+      onChange={(e) => setKeyword(e.target.value)}
+    />
+  );
+};
+
 const Students = () => {
+  const [searchInput, setInput] = useState("");
   const [students, setStudents] = useState([]);
+  const [filteredStudents, setFilteredStudents] = useState([]);
 
   useEffect(() => {
     getStudents().then((studentList) => {
       setStudents(studentList);
+      setFilteredStudents(studentList);
     });
   }, []);
 
+  const updateInput = async (input) => {
+    const filtered = students.filter((student) => {
+      return student.name.toLowerCase().includes(input.toLowerCase());
+    });
+    setInput(input);
+    setFilteredStudents(filtered);
+  };
+
   return (
     <>
-      <StyledGrid
+      <SearchArea>
+        <SearchBar input={searchInput} onChange={updateInput} />
+      </SearchArea>
+
+      <StudentsGrid
         container
         direction="row"
         justify="center"
         alignItems="center"
         spacing={10}
       >
-        {students.map(
+        {filteredStudents.map(
           ({
             id,
             name,
@@ -65,9 +106,14 @@ const Students = () => {
             );
           }
         )}
-      </StyledGrid>
+      </StudentsGrid>
     </>
   );
 };
 
 export default Students;
+
+SearchBar.propTypes = {
+  input: PropTypes.string,
+  onChange: PropTypes.func,
+};
