37bece0c48aedf40403fd529d0a28064db2aa1ac
João <jpsilva9898@gmail.com>
2021-01-14 22:56:30 +0000

    Send Transactions (#46)
    * Single Student Page
    
    * Page visual changes
    
    Adding material-ui components, and other CSS page aesthetics changes
    
    * connection with backend
    
    * fixing errors
    
    * moving progress bar to components
    
    * delete unused css file
    
    * communication with local backend instead of heroku
    
    * correct merging error
    
    * merge with main
    
    * Add service for loans
    
    * Change loans to loans.transactions
    
    * Some changes
    
    * Transactions
    
    * Send transactions
    
    * Some minor changes
    
    * Change fees to the correct value
    
    * Change file name
    
    Co-authored-by: tiago sousa <a81922@alunos.uminho.pt>

--- web/src/containers/students/Student.js
+++ web/src/containers/students/Student.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import ProgressBar from "../../components/Progress";
 import LoansService from "../../services/loans.service";
 import UserService from "../../services/user.service";
-import InvestmentService from "../../services/investment.service";
+import LoansTransactionsService from "../../services/loans-transactions.service";
 import { Euro, Create, School, Room } from "@material-ui/icons";
 import {
   Typography,
@@ -30,14 +30,6 @@ function Student(props) {
     setUserInfo(Info.user);
     console.log(Info.user);
   }, []);
-  let clickHandler=()=>{
-    let inv={
-      amount:tokens,
-      request:props.match.params.id
-    };
-    console.log(inv);
-    InvestmentService.newInvestment(inv);
-  };
   const matches = useMediaQuery("(min-width:600px)");
   const Box2 = withStyles({
     root: {
@@ -118,12 +110,19 @@ function Student(props) {
                   label="Tokens"
                   name="tokens"
                   variant="outlined"
-                  onChange={(e) => {
-                  setTokens(e.target.value)
-                  console.log(tokens);
+                  onChange={(e) => {                       
+                    e.target.value = !Number.isInteger(e.target.value)
+                      ? Math.floor(e.target.value)
+                      : e.target.value;
+                    setTokens(e.target.value);
                   }}
                 />
-                <Button variant="contained" color="primary" type="submit" onClick={clickHandler}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  onClick={() => LoansTransactionsService.provide_funds(props.match.params.id, tokens)}
+                >
                   Buy
                 </Button>
               </Box>
