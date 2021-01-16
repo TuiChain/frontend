6379854556d4fb17c4c1715c37ab132c644a1ae1
PedroFerr101 <pferreira59@gmail.com>
2021-01-16 00:20:41 +0000

    Add extra padding on forms in large screens

--- web/src/containers/authentication/Login.js
+++ web/src/containers/authentication/Login.js
@@ -12,11 +12,21 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import AuthService from "../../services/auth.service";
 
-const styles = {
+const styles = theme => ({
   fullWidth: {
     width: "100%",
   },
-};
+  form: {
+    [theme.breakpoints.only('md')]: {
+      paddingLeft: "10%",
+      paddingRight: "10%",
+    },
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: "15%",
+      paddingRight: "15%",
+    },
+  }
+});
 
 const Login = (props) => {
   const { classes, onLogin } = props;
@@ -59,7 +69,7 @@ const Login = (props) => {
         Login
       </Typography>
       <form onSubmit={formik.handleSubmit} className={classes.fullWidth}>
-        <Grid container spacing={2}>
+        <Grid className={classes.form} container spacing={2}>
           <Grid item xs={12}>
             <TextField
               error={formik.errors.username && formik.touched.username}
