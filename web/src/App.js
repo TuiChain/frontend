dc3de46c6058158d10896e9cc88b24b9e361f254
Nelson Sousa <36697946+nelsontss@users.noreply.github.com>
2021-01-16 13:42:31 +0000

    Kyc button (#41)
    * Stripe id verification button
    
    * Fix merge conflits
    
    * Remove unnecessary report
    
    * Pr fixes
    
    * Add prettier
    
    * Add return to service method
    
    * Merge v2

--- web/src/App.js
+++ web/src/App.js
@@ -16,6 +16,7 @@ import AuthService from "./services/auth.service";
 import WalletService from "./services/wallet.service";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
+import KycButton from "./containers/KycButton";
 
 const styles = {
   back: {
@@ -68,6 +69,7 @@ const App = (props) => {
                 component={LoanRequest}
                 wallet={wallet}
               />
+              <Route auth={auth} path="/kyc" render={() => <KycButton />} />
               <Route path="/login">
                 {auth ? <Redirect to="/" /> : <Login onLogin={handlerLogin} />}
               </Route>
---
+++ web/src/components/Modal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal(props) {
+  const classes = useStyles();
+
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      className={classes.modal}
+      open={props.open}
+      onClose={props.handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={props.open}>{props.children}</Fade>
+    </Modal>
+  );
+}
+
+TransitionsModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  children: PropTypes.element.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
