15e78b2550b281f0e907fb233757c2c13b5bde38
João <jpsilva9898@gmail.com>
2021-01-18 01:50:13 +0000

    Generate unique student token image (#59)
    * Add dependencies
    
    * Add token image
    
    * Generate token image
    
    * Run format
    
    * Change comment
    
    * Some changes
    
    * Last changes
    
    * Resolve merge locally

---
+++ web/src/services/token-image.service.js
@@ -0,0 +1,48 @@
+import jazzicon from "@metamask/jazzicon";
+import Constants from "../constants";
+
+/**
+ * Function that generates a unique token image for a token address
+ *
+ * @param { String } tokenAddress String which represents the address of the token
+ *
+ * @returns SVG string which represents the image
+ */
+function generateTokenImage(tokenAddress) {
+  let svgDiameter = 1194;
+  let capSize = 814;
+
+  let capBase64 = Constants.logo_png;
+  let identicon = jazzicon(
+    svgDiameter,
+    Number(BigInt(tokenAddress) & 0xffffffffn)
+  );
+  let svg = identicon.childNodes[0];
+
+  let background = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "rect"
+  );
+  background.setAttribute("width", svgDiameter);
+  background.setAttribute("height", svgDiameter);
+  background.setAttribute("fill", identicon.style.background);
+
+  let cap = document.createElementNS("http://www.w3.org/2000/svg", "image");
+  cap.setAttribute("href", capBase64);
+  cap.setAttribute("x", (svgDiameter - capSize) / 2);
+  cap.setAttribute("y", (svgDiameter - capSize) / 2);
+  cap.setAttribute("width", capSize);
+  cap.setAttribute("height", capSize);
+  cap.setAttribute("preserveAspectRatio", "xMidYMid meet");
+
+  svg.prepend(background);
+  svg.append(cap);
+
+  let svgString = new XMLSerializer().serializeToString(svg);
+
+  return "data:image/svg+xml;base64," + btoa(svgString);
+}
+
+export default {
+  generateTokenImage,
+};
