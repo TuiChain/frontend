3c525aa3a726d6b9fe2467e7ffe648001143e85e
Pedro Moreira <pedrombm19@gmail.com>
2021-01-24 00:41:20 +0000

    move LoanRequest -> /loans

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ import Login from "./containers/authentication/Login";
 import Signup from "./containers/authentication/Signup";
 import Loan from "./containers/loans/Loan";
 import Loans from "./containers/loans/Loans";
-import LoanRequest from "./containers/LoanRequest";
+import LoanRequest from "./containers/loans/LoanRequest";
 import LoanRequests from "./containers/admin/LoanRequests";
 import Error from "./containers/Error";
 import { ThemeProvider, withStyles } from "@material-ui/core/styles";
